f7c4b60036dc21bcdcb842d9ef4e498f3c8676f4
verdesotostxvenn-ctrl <verdesotostxvenn@gmail.com>
2026-05-09 04:37:27 -0500

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,9 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Extraemos las coordenadas de energía del entorno de Vite
-// Usamos "as string" para que TypeScript no bloquee el radar
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+/**
+ * Táctica de Blindaje: Cast al tipo 'any' para evitar que TypeScript 
+ * bloquee el acceso a las variables de entorno durante el build en Netlify.
+ */
+const supabaseUrl = (import.meta as any).env.VITE_SUPABASE_URL as string;
+const supabaseAnonKey = (import.meta as any).env.VITE_SUPABASE_ANON_KEY as string;
 
 // Creamos el puente satelital con Supabase
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
