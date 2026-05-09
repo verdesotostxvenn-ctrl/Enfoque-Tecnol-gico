be3708837fc311533a5169c75504236af3f9b4f9
verdesotostxvenn-ctrl <verdesotostxvenn@gmail.com>
2026-05-09 04:41:12 -0500

    Delete src/lib/supabase.ts

--- src/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://iordxjvbxtytcraepmnd.supabase.co';
-const supabaseAnonKey = 'sb_publishable_B0D4_ccnf1wIPx9YJUacmw_kLJuIu-g';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
