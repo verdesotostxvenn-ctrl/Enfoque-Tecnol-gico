8e2d7c8c5f4496537dfbffad05992c4f51bfb768
verdesotostxvenn-ctrl <verdesotostxvenn@gmail.com>
2026-05-08 22:04:02 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import Lobby from './components/Lobby'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Lobby from './pages/Lobby';
+import Hub from './pages/Hub';
 
 function App() {
   return (
-    <main className="min-h-screen bg-black">
-      <Lobby />
-    </main>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Lobby />} />
+        <Route path="/hub" element={<Hub />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
