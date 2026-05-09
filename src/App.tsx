196b2b20c676f0dbe2ca5943f8f1c6bfa0b920e5
verdesotostxvenn-ctrl <verdesotostxvenn@gmail.com>
2026-05-08 22:06:09 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Lobby from './pages/Lobby';
-import Hub from './pages/Hub';
+import Lobby from './components/Lobby';
+import Hub from './components/Hub';
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Página inicial en la carpeta components */}
         <Route path="/" element={<Lobby />} />
+        {/* Página del Hub en la carpeta components */}
         <Route path="/hub" element={<Hub />} />
       </Routes>
     </Router>
