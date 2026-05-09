fb8337d72c7a79dbbcb35ff63fb20599d6cd9bf5
verdesotostxvenn-ctrl <verdesotostxvenn@gmail.com>
2026-05-08 22:56:12 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Lobby from './components/Lobby';
 import Hub from './components/Hub';
+// Importamos las nuevas misiones
+import MisionVolcan from './components/MisionVolcan';
+import MisionInundacion from './components/MisionInundacion';
+import MisionEvacuacion from './components/MisionEvacuacion';
 
 function App() {
   return (
@@ -9,8 +13,14 @@ function App() {
       <Routes>
         {/* Registro inicial */}
         <Route path="/" element={<Lobby />} />
-        {/* Panel de misiones */}
+        
+        {/* Panel principal */}
         <Route path="/hub" element={<Hub />} />
+
+        {/* Direcciones de las misiones individuales */}
+        <Route path="/volcan" element={<MisionVolcan />} />
+        <Route path="/inundacion" element={<MisionInundacion />} />
+        <Route path="/evacuacion" element={<MisionEvacuacion />} />
       </Routes>
     </Router>
   );
