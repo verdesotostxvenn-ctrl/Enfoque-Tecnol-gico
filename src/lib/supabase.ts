5702a55f3aff8a24e81a8936343105f07768dae4
verdesotostxvenn-ctrl <verdesotostxvenn@gmail.com>
2026-05-08 19:54:37 -0500

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Conexión oficial del Proyecto Fusilero
 const supabaseUrl = 'https://iordxjvbxtytcraepmnd.supabase.co';
 const supabaseAnonKey = 'sb_publishable_B0D4_ccnf1wIPx9YJUacmw_kLJuIu-g';
 
