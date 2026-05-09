8e526753fcbc0d05a259959ca654ae6f1b646b09
verdesotostxvenn-ctrl <verdesotostxvenn@gmail.com>
2026-05-09 03:57:00 -0500

    Create supabaseClient.ts

---
+++ src/supabaseClient.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-api-js';
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
